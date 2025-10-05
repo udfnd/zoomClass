03973b02334225b2b746b5ef2c135f68ce181323
Seungmok Han <32510116+udfnd@users.noreply.github.com>
2025-10-05 23:57:56 +0900

    Fix packaged renderer bundle filename

--- zoom-video-app/webpack.renderer.config.js
+++ zoom-video-app/webpack.renderer.config.js
@@ -70,7 +70,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '.webpack/renderer'),
-    filename: 'renderer.js',
+    filename: 'index.js',
     globalObject: 'window',
     publicPath: './',
   },
